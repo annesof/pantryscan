1e25f316a0d6514eeb852a165c49b407ffe1f9ea
annesof <asdeguelle@gmail.com>
2023-03-28 11:00:05 +0200

    retry and timeout

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   e2e: {
     defaultCommandTimeout: 6000,
+    retries: 1,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
